e0bbdee4797911a475a60cfc8f5ac9789d3986c3
Christian Schaiter <christian.schaiter@outlook.com>
2017-06-21 19:12:39 +0200

    Initial commit containing the angular project structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import './rx-addons';
+import 'hammerjs';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpModule, RequestOptions } from '@angular/http';
+
+import { AlertModule } from 'ng2-bootstrap';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MomentModule } from 'angular2-moment';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpModule, FlexLayoutModule.forRoot(), ReactiveFormsModule, RouterModule.forRoot(rootRouterConfig), MomentModule,
+    AlertModule.forRoot()
+  ],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  
+  constructor() {}
+}
---
+++ src/app/routing/routingParams.ts
@@ -0,0 +1,5 @@
+export  class RoutingParams {
+
+    public static BOX_NAME : string = "boxName";
+
+}
