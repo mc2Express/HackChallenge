f0b185256fdc9250bc4a04f0c38a66a5e02dd24e
Christoph Aichinger <christoph.aichinger@world-direct.at>
2017-06-23 01:12:13 +0200

    additional fields in frontend dto

--- Backend.Net/BusinessDomain/typescript/Contract.ts
+++ Backend.Net/BusinessDomain/typescript/Contract.ts
@@ -1,16 +1,20 @@
 
 
- 
 
-    export interface Contract { 
-        contractNr: string;
-        spContractDescription: string;
-        creationDate: string;
-        offertDate: string;
-        offertNr: string;
-        transportClasses: TransportClass[];
-        railwayBills: RailwayBill[];
-        invoices: IncomingInvoicesConfirmations[];
-        wagons: Wagon[];
-    
+
+export interface Contract {
+	contractNr: string;
+	spContractDescription: string;
+	creationDate: string;
+	offertDate: string;
+	offertNr: string;
+	offertTextIntern: string;
+	offertOrganisation: string;
+	sumWagen: string;
+	sumTonnen: string;
+	transportClasses: TransportClass[];
+	railwayBills: RailwayBill[];
+	invoices: IncomingInvoicesConfirmations[];
+	wagons: Wagon[];
+
 }
--- Backend.Net/BusinessDomain/typescript/RailwayBill.ts
+++ Backend.Net/BusinessDomain/typescript/RailwayBill.ts
@@ -1,19 +1,20 @@
 
 
- 
 
-    export interface RailwayBill { 
-        forwardingNumber: string;
-        forwardingStation: TrainStation;
-        takeOverStation: TrainStation;
-        receivingStation: TrainStation;
-        deliveryStation: TrainStation;
-        takeOverDate: string;
-        issueDate: string;
-        issueLocation: string;
-        descriptions: string[];
-        classOfReservation: string;
-        reservationNumber: string;
-        wagon: Wagon;
-    
+
+export interface RailwayBill {
+	gvId: string;
+	forwardingNumber: string;
+	forwardingStation: TrainStation;
+	takeOverStation: TrainStation;
+	receivingStation: TrainStation;
+	deliveryStation: TrainStation;
+	takeOverDate: string;
+	issueDate: string;
+	issueLocation: string;
+	descriptions: string[];
+	classOfReservation: string;
+	reservationNumber: string;
+	wagon: Wagon;
+
 }
