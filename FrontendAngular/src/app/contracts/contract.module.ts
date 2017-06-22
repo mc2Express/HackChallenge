ca64960f28f705d79815043e68cecf379256e516
Christian Schaiter <christian.schaiter@outlook.com>
2017-06-22 23:40:53 +0200

    style frontend

--- FrontendAngular/src/app/contracts/contract.module.ts
+++ FrontendAngular/src/app/contracts/contract.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 
 // This Module's Components
 import { ContractTableComponent } from './contract-table/contract-table.component';
-import { ContractDetailComponent } from "app/contracts/contract-detail/contract-detail.component";
+import { ContractRowComponent } from "app/contracts/contract-row/contract-row.component";
 import { ContractService } from "app/contracts/shared/contract.service";
 import { CommonModule } from "@angular/common";
+import { ContractDetailComponent } from "app/contracts/contract-detail/contract-detail.component";
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        ContractTableComponent, ContractDetailComponent
+        ContractTableComponent, ContractRowComponent, ContractDetailComponent
     ],
     exports: [
-        ContractTableComponent, ContractDetailComponent
+        ContractTableComponent, ContractRowComponent, ContractDetailComponent
     ],
     providers: [ContractService]
 })
--- FrontendAngular/src/app/contracts/shared/contract.service.ts
+++ FrontendAngular/src/app/contracts/shared/contract.service.ts
@@ -8,29 +8,39 @@ import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ContractService {
-  
-  constructor(private http: Http) { }
+    
+    public contract: Contract;
 
-  search(): Observable<Contract[]> {
-      return Observable.of([
-          <Contract>{
-            contractNr: "2000",
-            creationDate: "Today",
-            invoices: undefined,
-            offertDate: "Tomorrow",
-            offertNr: "2324",
-            railwayBills: [],
-            transportClasses: [],
-            wagons: []
-          }
-      ])
+    constructor(private http: Http) { }
 
-    // return this.http
-    //   .get(`http://localhost:53852/SupplyChain/contracts`)
-    //   .map((r: any) => r.json() as Contract[])
-    //   .catch((error: any) => {
-    //       console.error('An friendly error occurred', error);
-    //       return Observable.throw(error.message || error);
-    //   });
-  }
+    search(): Observable<Contract[]> {
+        return Observable.of([
+            <Contract>{
+                contractNr: "2000",
+                creationDate: "Today",
+                invoices: undefined,
+                offertDate: "Tomorrow",
+                offertNr: "2324",
+                railwayBills: [],
+                transportClasses: [],
+                wagons: []
+            }
+        ])
+
+        // return this.http
+        //   .get(`http://localhost:53852/SupplyChain/contracts`)
+        //   .map((r: any) => r.json() as Contract[])
+        //   .catch((error: any) => {
+        //       console.error('An friendly error occurred', error);
+        //       return Observable.throw(error.message || error);
+        //   });
+    }
+
+    public setCurrentContract(contract: Contract): void {
+        this.contract = contract;
+    }
+
+    public clearCurrentContract(): void {
+        this.contract = undefined;
+    }
 }
