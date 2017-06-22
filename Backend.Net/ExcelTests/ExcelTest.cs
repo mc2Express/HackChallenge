2dff1bc13792467b5e31b82e49563044cb205c11
feitzi <mathias@feitzinger.org>
2017-06-22 20:06:35 +0200

    relationInfos parser added

--- Backend.Net/ExcelTests/ExcelTest.cs
+++ Backend.Net/ExcelTests/ExcelTest.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Parse.Impl;
+﻿using System.Linq;
+using Infrastructure.Parse.Impl;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ExcelTests
@@ -14,7 +15,7 @@ namespace ExcelTests
                 @"C:\Users\mathi\Desktop\ÖBB Hackathon 22. und 23.06.2017\2_Purchase contracts from supplier LugoTerminal\Incoming external purchase from supplier LugoT_excel.xlsx";
             var result = excelImporter.Parse(path);
 
-            Assert.AreEqual(24, result.Count);
+            Assert.AreEqual(24, result.Count());
         }
 
         [TestMethod]
@@ -25,7 +26,7 @@ namespace ExcelTests
                 @"C:\Users\mathi\Desktop\ÖBB Hackathon 22. und 23.06.2017\4_Reservation train lists for LugoShuttle\4_Train lists for Lugo Shuttle 2016  November_gekürzt.xls";
              var result = excelImporter.Parse(path);
 
-            Assert.AreEqual(16, result.Count);
+            Assert.AreEqual(16, result.Count());
         }
 
         [TestMethod]
@@ -36,7 +37,7 @@ namespace ExcelTests
                 @"C:\Users\mathi\Desktop\ÖBB Hackathon 22. und 23.06.2017\6_Incoming external invoices from LugoTerminal\Incoming invoices_confirmation of supplier LugoTerminal_Excel.xlsx";
             var result = excelImporter.Parse(path);
 
-            Assert.AreEqual(67, result.Count);
+            Assert.AreEqual(67, result.Count());
 
         }
 
@@ -51,7 +52,20 @@ namespace ExcelTests
                 @"C:\Users\mathi\Desktop\ÖBB Hackathon 22. und 23.06.2017\5_Wagon status message list\Wagen status message list_RTLM-548_Hackathon-Statusmeldungen_additional infos.xlsx";
             var result = excelImporter.Parse(path);
 
-            Assert.AreEqual(2591, result.Count);
+            Assert.AreEqual(2591, result.Count());
+
+        }
+
+        
+        [TestMethod]
+        public void RelationInfoParserTest()
+        {
+            var excelImporter = new RelationInfoParser();
+            var path =
+                @"C:\Users\mathi\Desktop\ÖBB Hackathon 22. und 23.06.2017\5_Wagon status message list\Wagen status message list_RTLM-548_Hackathon-Statusmeldungen_additional infos.xlsx";
+            var result = excelImporter.Parse(path);
+
+            var count = result.LookupStatus.Count();
 
         }
     }
