bd87144819e4e596520d03add7a554a5101dcb99
Christoph Aichinger <christoph.aichinger@world-direct.at>
2017-06-23 01:37:42 +0200

    set railwayBills in transportClasses during enrichment

--- Backend.Net/WebApi/Global.asax.cs
+++ Backend.Net/WebApi/Global.asax.cs
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
-=======
-﻿using System.Web.Mvc;
->>>>>>> 02d8369d4c5274a86039e9f946b917038c358699
 using System.Web.Routing;
 
 namespace WebApi
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+	public class MvcApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
 }
